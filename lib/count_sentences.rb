55b9118fc003c6868ae1e2d6052b0cc404570eb6
William Walsh <wmdwalsh0@gmail.com>
2021-08-26 09:26:48 -0400

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else 
+      return false
+    end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    else 
+      return false
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true 
+    else 
+      return false
+    end 
   end
 
   def count_sentences
+    # if self.sentence?
+    #   return true
+    # else 
+    #   return 0
+    # end
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
 
   end
 end
